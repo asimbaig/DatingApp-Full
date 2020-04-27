f31a967fb80c13794075604b38e82b3d159d077a
asim baig <as_baig@yahoo.com>
2020-04-27 18:05:28 +0100

    End of section 12

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -20,6 +20,7 @@ namespace DatingApp.API.Helpers
 			CreateMap<Photo, PhotosForDetailedDto>();
 			CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotoForCreationDto, Photo>();
+			CreateMap<UserForRegisterDto, User>();
 		}
     }
 }
